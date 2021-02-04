c1b005853c34875f43ea443b5da68e52c17dbedf
AngelovskiTeodor <angelovski.teo@outlook.com>
2021-02-04 17:29:11 +0100

    Added initial Spring Boot Modules for aggregates: Article, Comment and reaction

---
+++ article/src/test/java/mk/ukim/finki/emt/onlinemagazine/article/ArticleApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.onlinemagazine.article;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
