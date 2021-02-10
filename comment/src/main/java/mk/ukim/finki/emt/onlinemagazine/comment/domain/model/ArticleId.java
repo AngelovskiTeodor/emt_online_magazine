ba7821e3c6255a8071276fa8dee9a9cf52602320
AngelovskiTeodor <angelovski.teo@outlook.com>
2021-02-10 11:27:13 +0100

    Added Comment Repository and Service. Bug fixes and refactoring in Comment Aggregate.

---
+++ comment/src/main/java/mk/ukim/finki/emt/onlinemagazine/comment/domain/model/ArticleId.java
@@ -0,0 +1,21 @@
+package mk.ukim.finki.emt.onlinemagazine.comment.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.Getter;
+import mk.ukim.finki.emt.onlinemagazine.sharedkernel.domain.base.DomainObjectId;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+public class ArticleId extends DomainObjectId {
+
+    protected ArticleId() {
+        super(DomainObjectId.randomId(ArticleId.class).toString());
+    }
+
+    @JsonCreator
+    public ArticleId(String id) {
+        super(id);
+    }
+}
--- comment/src/main/java/mk/ukim/finki/emt/onlinemagazine/comment/domain/model/Comment.java
+++ comment/src/main/java/mk/ukim/finki/emt/onlinemagazine/comment/domain/model/Comment.java
@@ -22,6 +22,9 @@ public class Comment extends AbstractEntity<CommentId> {
     private boolean deleted = false;
 
     @Embedded
+    private ArticleId articleId;
+
+    @Embedded
     private Author author;
 
     @Embedded
@@ -33,4 +36,79 @@ public class Comment extends AbstractEntity<CommentId> {
     @Embedded
     private Popularity popularity;
 
+    public Comment() {
+    }
+
+    public Comment(ArticleId articleId, Author author, PostingDate commentDate, TextContent textContent, Popularity popularity) {
+        this.articleId = articleId;
+        this.author = author;
+        this.commentDate = commentDate;
+        this.textContent = textContent;
+        this.popularity = popularity;
+    }
+
+    public Comment(CommentId id, ArticleId articleId, Author author, PostingDate commentDate, TextContent textContent, Popularity popularity) {
+        super(id);
+        this.articleId = articleId;
+        this.author = author;
+        this.commentDate = commentDate;
+        this.textContent = textContent;
+        this.popularity = popularity;
+    }
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public ArticleId getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(ArticleId articleId) {
+        this.articleId = articleId;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public PostingDate getCommentDate() {
+        return commentDate;
+    }
+
+    public void setCommentDate(PostingDate commentDate) {
+        this.commentDate = commentDate;
+    }
+
+    public TextContent getTextContent() {
+        return textContent;
+    }
+
+    public void setTextContent(TextContent textContent) {
+        this.textContent = textContent;
+    }
+
+    public Popularity getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(Popularity popularity) {
+        this.popularity = popularity;
+    }
 }
